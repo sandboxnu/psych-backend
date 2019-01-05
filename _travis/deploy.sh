9550bbcbc7a48f6ee29d60522d10b41426b907be
dajinchu <dajinchu@gmail.com>
2019-01-05 02:00:20 -0500

    Trying to get travis pushing to DO working

--- _travis/deploy.sh
+++ _travis/deploy.sh
@@ -2,6 +2,8 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
+  chmod 600 sandbox-deploy-key
+  mv sandbox-deploy-key ~/.ssh/id_rsa
   git remote add deploy "deploy@142.93.49.129:/var/www/psych-backend/src"
   git config user.name "Travis CI"
   git config user.email "dajinchu+travisCI@gmail.com"
--- _travis/install.sh
+++ _travis/install.sh
@@ -4,4 +4,4 @@ set -x # Show the output of the following commands (useful for debugging)
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_9543970147d9_key -iv $encrypted_22009518e18d_key -in sandbox-deploy-key.enc -out sandbox-deploy-key -d
 chmod 600 sandbox-deploy-key
-mv sandbox-deploy-key ~/.ssh/id_rsa
+# mv sandbox-deploy-key ~/.ssh/id_rsa
