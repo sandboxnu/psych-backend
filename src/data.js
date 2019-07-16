254fffb18cd2bb2cca22f541af7717e7b52a61f2
Yash Shetty <yash17shetty@gmail.com>
2019-07-15 23:25:55 -0700

    ys - configure express router for GET/POST on /data
    using mongoose to save and reteive data as mongodb documents

--- src/data.js
+++ src/data.js
@@ -5,40 +5,55 @@ const sanitize = require('sanitize-filename');
 const unusedFilename = require('unused-filename');
 const { DATADIR } = require('./dirs');
 const { authMiddleware } = require('./authentication');
+const fs = require('fs');
+const { timeStamp, completionCode } = require('./serverUtils');
+
+var DataRecord = require('./models/DataRecord.model');
+
+const collection = 'experiment_data'
 
 module.exports = (router = new Router()) => {
   router.get('/', authMiddleware);
   router.get('/', (req, res) => {
-    res.status(200).set({
-      'Content-Type': 'application/zip',
-      'Content-disposition': 'attachment; filename=data.zip',
+    DataRecord.find({}).then(function(dataRecords) {
+      var response = {};
+      var count = 1;
+      dataRecords.forEach(function(record) { 
+        response[count] = JSON.parse(record["data"]);
+        count += 1;
+      });
+      return Promise.all([response]);
+    }).then(function(respArray) {
+      res.status(200).set({
+      'Content-Type': 'application/json',
+      });
+      res.send(respArray[0]);
+    }).catch(function(error) {
+      res.status(500).send(error);
     });
-    const zip = archiver('zip');
-    zip.pipe(res);
-    zip.directory(DATADIR, false);
-    zip.finalize();
   });
 
   router.post('/', (req, res) => {
-    const { files: { file } = {} } = req;
-    if (!file) {
-      return res.status(400).send('Collected data file was not uploaded!');
+    const jsonData = req.body
+    if (Object.keys(jsonData).length === 0) {
+      return res.status(400).send('Malformed or bad data');
     }
-    if (!file.name) {
-      return res.status(400).send('Collected data file missing name');
-    }
-    // Sanitize filename to ensure it's a valid path (and avoid certain hacks).
-    const safeName = sanitize(file.name);
-    unusedFilename(path.join(DATADIR, safeName))
-      .then((filename) => {
-        // Store the file on the server.
-        file.mv(filename, (err) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-          return res.send('Data saved');
-        });
-      });
+    //we need the front end to tell us which config it used to run the experiment, since 
+    //the researcher probably wants to know this to make sense of the data.
+    const configID = jsonData['configID']
+    const time = timeStamp()
+    //some problem with this completion code
+    //const completionID = completionCode()
+    const completionID = "abcd";
+
+    const data = new DataRecord({time: time, completionID: completionID, configID: configID, data: JSON.stringify(jsonData)});
+    data.save(function (err, fluffy) {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err);
+      }
+      return res.send('Data saved');
+    });
     return false;
   });
   return router;
